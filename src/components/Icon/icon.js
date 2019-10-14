5db131caa7c5417306b9776a2f3215e258a47136
Rodion Mescheryakov <msrodion@gmail.com>
2019-10-14 18:44:02 +0300

    Update `Icon` component

--- src/components/Icon/icon.js
+++ src/components/Icon/icon.js
@@ -3,10 +3,10 @@ import PropType from 'prop-types';
 import './icon.css';
 import { ICONS, PATHS } from './icons';
 
-const Icon = ({ name }) => {
+const Icon = ({ className, name }) => {
   return (
     <svg
-      className="icon"
+      className={`icon ${className || ''}`}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 48 48"
       dangerouslySetInnerHTML={{ __html: PATHS[name] }}
